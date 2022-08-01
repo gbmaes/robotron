60b9ad790c2df6a18c69a98dc863ac5eadf4f9ab
Gabriel Maes <gabrielmaes@hotmail.com>
2022-08-01 20:17:21 +0200

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
-const robotron = document.querySelector('#robotron');
+const subtrair = document.querySelector('#subtrair');
+const somar = document.querySelector('#somar');
+const braco = document.querySelector('#braco');
 
-robotron.addEventListener("click", (event) => {
-    console.log(event)
-})
+const controle = document.querySelectorAll(".controle-ajuste")
+console.log(controle)
 
-function dizOi(nome) {
-    console.log("Oi" + nome)
-    console.log("Bem vindo ao robotron 2000")
+somar.addEventListener("click", () => {
+manipulaDados("somar")})
+
+subtrair.addEventListener("click", () => {manipulaDados("subtrair")})
+
+function manipulaDados(operacao){
+    if (operacao === "subtrair"){
+        braco.value = parseInt(braco.value) - 1
+    } else {
+        braco.value = parseInt(braco.value) + 1
+    }
 }
-
-dizOi("Gabriel")
