13f3f48275ca1bae34e56a1e36a7e79e34bf7c9c
Gabriel Maes <gabrielmaes@hotmail.com>
2022-08-02 14:23:47 +0200

    update main.js

--- js/main.js
+++ js/main.js
@@ -5,13 +5,19 @@ const braco = document.querySelector('#braco');
 const controle = document.querySelectorAll(".controle-ajuste")
 console.log(controle)
 
-somar.addEventListener("click", () => {
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (event) => {
+            manipulaDados(event.target.textContext)
+    })
+} )
+
+/*somar.addEventListener("click", () => {
 manipulaDados("somar")})
 
-subtrair.addEventListener("click", () => {manipulaDados("subtrair")})
+subtrair.addEventListener("click", () => {manipulaDados("subtrair")})*/
 
 function manipulaDados(operacao){
-    if (operacao === "subtrair"){
+    if (operacao === "-"){
         braco.value = parseInt(braco.value) - 1
     } else {
         braco.value = parseInt(braco.value) + 1
