da7bcdb74d007c93920a30632795a8a341047a74
Gabriel Maes <gabrielmaes@hotmail.com>
2022-08-05 22:41:34 +0200

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
 const subtrair = document.querySelector('#subtrair');
 const somar = document.querySelector('#somar');
-const braco = document.querySelector('#braco');
+
+
 const controle = document.querySelectorAll(".controle-ajuste")
 console.log(controle)
 
 controle.forEach( (elemento) => {
-    elemento.addEventListener("click", (event) => {
-            manipulaDados(event.target.textContext)
+    elemento.addEventListener("click", (evento) => {
+            manipulaDados(evento.target.textContext, evento.target.parentNode)
+            
     })
 } )
+
 /*somar.addEventListener("click", () => {
 manipulaDados("somar")})
+
 subtrair.addEventListener("click", () => {manipulaDados("subtrair")})*/
-function manipulaDados(operacao){
+
+function manipulaDados(operacao, controle){
+    const peca = controle.querySelector(".controle-contador")
+
     if (operacao === "-"){
-        braco.value = parseInt(braco.value) - 1
+        peca.value = parseInt(braco.value) - 1
     } else {
         braco.value = parseInt(braco.value) + 1
     }
